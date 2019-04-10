1cb1f69b5730bca18950a654981d2b378d0870ad
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-04-10 22:43:55 +0200

    Sorted hotkeys

--- KeymapXmlParser.php
+++ KeymapXmlParser.php
@@ -23,23 +23,57 @@ class KeymapXmlParser
         if ($xmlDefintion['parent']) {
             $keymapParent = (string) $xmlDefintion['parent'];
         }
+        $stmt = $this->db->prepare('INSERT INTO keymaps VALUES (:name, :parent)');
+        $stmt->execute(
+            [
+                'name' => $keymapName,
+                'parent' => $keymapParent ?? null,
+            ]
+        );
         foreach ($xmlDefintion->action as $action) {
             $item = [
                 'name' => $keymapName,
-                'parent' => $keymapParent ?? null
             ];
             $item['id'] = (string) $action['id'];
             if ($action->{'keyboard-shortcut'}) {
-                $item['shortcut'] = (string) $action->{'keyboard-shortcut'}['first-keystroke'];
+                $item['shortcut'] = $this->shortcutSort((string) $action->{'keyboard-shortcut'}['first-keystroke']);
             } elseif ($action->{'mouse-shortcut'}) {
                 $item['shortcut'] = strtolower((string) $action->{'keyboard-shortcut'}['keystroke']);
             }
 
-            $stmt = $this->db->prepare('INSERT INTO shortcuts VALUES (:id, :shortcut, :name, :parent)');
+            $stmt = $this->db->prepare('INSERT INTO shortcuts VALUES (:id, :shortcut, :name)');
             $stmt->execute($item);
             if ($i++ > 100) {
                 return;
             }
         }
+    }
+
+    private function shortcutSort(string $shortcut)
+    {
+        $shortcut = strtolower($shortcut);
+        $from = $shortcut;
+        $keyorder = [
+            'control' => 1,
+            'alt' => 2,
+            'altgr' => 2,
+            'shift' => 3,
+        ];
+        $keys = explode(' ', $shortcut);
+        usort($keys, function ($key1, $key2) use ($keyorder) {
+            $key1Val = $key2Val = 999;
+            if (array_key_exists($key1, $keyorder)) {
+                $key1Val = $keyorder[$key1];
+            }
+            if (array_key_exists($key2, $keyorder)) {
+                $key2Val = $keyorder[$key2];
+            }
+            return $key1Val <=> $key2Val;
+        });
+        $to = implode(' ', $keys);
+        if ($from !== $to) {
+            echo sprintf('From %s'.PHP_EOL.'  to %s' . PHP_EOL, $from, $to);
+        }
+        return $to;
     }
 }
--- index.php
+++ index.php
@@ -13,10 +13,14 @@ $db->exec('
 CREATE TABLE IF NOT EXISTS shortcuts (
     id VARCHAR(255), 
     shortcut VARCHAR(255), 
-    keymapName VARCHAR(255), 
-    keymapParent VARCHAR(255) NULL 
-) 
+    keymapName VARCHAR(255) 
+)
 ');
+$db->exec('DROP TABLE IF EXISTS keymaps');
+$db->exec('CREATE TABLE keymaps (
+    name VARCHAR(255), 
+    parent VARCHAR(255)
+)');
 $i = 0;
 
 $keymapFiles = [
@@ -26,8 +30,11 @@ $keymapFiles = [
 ];
 
 foreach ($keymapFiles as $file) {
-    $default = simplexml_load_string(file_get_contents($file));
-    (new KeymapXmlParser($db))->parseXmlDefinition($default, $db, $i);
+    $keymapXml = simplexml_load_string(file_get_contents($file));
+    (new KeymapXmlParser($db))->parseXmlDefinition($keymapXml);
+    echo 'Loaded ' . $file . PHP_EOL;
 }
+
+
 
 
